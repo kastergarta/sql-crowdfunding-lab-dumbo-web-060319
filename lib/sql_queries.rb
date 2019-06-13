6d9933e53cb00003bab26354cb8b95e37bda6f4f
Dennis <dennis.basyrov@gmail.com>
2019-06-13 23:13:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,6 +22,13 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT users.name, SUM(pledges.amount) AS total_pledges FROM users INNER JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY total_pledge;"
 end
 
+"SELECT users.name, SUM(pledges.amount) AS total_pledges 
+FROM users
+INNER JOIN pledges
+  ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY total_pledges;"
+
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "Write your SQL query Here"
 end
