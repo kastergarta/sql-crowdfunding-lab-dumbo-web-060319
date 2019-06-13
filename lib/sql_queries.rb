65d46ec647950f130725b0e16105395d731b6e1a
Dennis <dennis.basyrov@gmail.com>
2019-06-13 22:51:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,13 +18,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING over_goal >= 0;"
 end
 
-# "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal 
-# FROM projects
-# INNER JOIN pledges
-# ON projects.id = pledges.project_id
-# GROUP BY projects.title
-# HAVING over_goal >= 0;"
-
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "Write your SQL query Here"
 end
